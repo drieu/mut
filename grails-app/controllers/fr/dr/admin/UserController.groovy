e69e6f383d38f6e9827f5ed6e7ea7fda484246f9
drieu <damien.rieu@gmail.com>
2013-06-03 15:44:26 +0200

    - Add user admin web pages.

---
+++ grails-app/controllers/fr/dr/admin/UserController.groovy
@@ -0,0 +1,8 @@
+package fr.dr.admin
+
+import fr.dr.model.User
+
+class UserController {
+
+    def scaffold = User
+}
--- grails-app/domain/fr/dr/model/User.groovy
+++ grails-app/domain/fr/dr/model/User.groovy
@@ -2,6 +2,10 @@ package fr.dr.model
 
 class User {
 
+    String login
+
+    String password
+
     static constraints = {
     }
 }
